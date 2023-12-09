e768c6c46bdeef9ba8d4e9690004c6c09305a599
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-12-08 20:00:54 -0500

    Created basic console app to practice with OOP

---
+++ oopPractice1/Dog.cpp
@@ -0,0 +1,16 @@
+#include "Dog.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+void Dog::shake() {
+	cout << "Dog shakes..." << endl;
+}
+
+void Dog::sit() {
+	cout << "Dog sits..." << endl;
+}
+
+void Dog::layDown() {
+	cout << "Dog lays down..." << endl;
+}
---
+++ oopPractice1/Dog.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+using namespace std;
+class Dog
+{
+public:
+	string breed;
+	string color;
+	float height;
+	float weight;
+
+	void shake();
+	void sit();
+	void layDown();
+};
+
